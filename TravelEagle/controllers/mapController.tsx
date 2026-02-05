9724d95e2f40162c150ef13080a0069449d50e00
jbnewman27 <jbarilne@nyit.edu>
2026-02-05 18:02:01 -0500

    Added GooglePlacesAutoComplete, Altered File Structure for functions(Made Controller File), Added search bar from GPAC and BottomSheet to MapView, added functions for fittingMarkers to screen, getting place info from searching

---
+++ TravelEagle/controllers/mapController.tsx
@@ -0,0 +1,30 @@
+export const animateToRegion = (mapRef, lat, lng) =>{
+    mapRef.current.animateToRegion(
+              {
+                latitude: lat,
+                longitude: lng,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              },
+              500,
+            );
+}
+export const goToSelectedPlace = (mapRef, details, setSelectedPlace) => {
+
+        const name = details.name;
+        const lng = details.geometry.location.lng;
+        const lat = details.geometry.location.lat;
+    setSelectedPlace({
+        name, lng, lat
+    });
+
+    animateToRegion (mapRef, lat, lng)
+};
+export const fitMarkerstoScreen  = (mapRef, places) => {
+    const coordinates = places.map(place => ({latitude: place.lat, longitude: place.lng }));
+
+    mapRef.current.fitToCoordinates(coordinates, {
+        edgePadding: {top: 70, right: 70, bottom: 70, left: 70},
+        animated: true,
+        });
+    };
