e292bec08e0779ae92a692bff18c2b54acbcbc01
Tezbir <tezbir1235@yahoo.com>
2026-04-10 00:00:11 -0400

    fixed oversight in reverse geolocation

--- TravelEagle/LocationServices/liveLocation.js
+++ TravelEagle/LocationServices/liveLocation.js
@@ -6,7 +6,7 @@ const useLocation = () => {
     const [errorMsg, setErrorMsg] = useState(null);
     const [latitude, setLatitude] = useState(null);
     const [longitude, setLongitude] = useState(null);
-
+    const [address, setAddress] = useState(null)//for when user wants to post, dont have to type out address
 
 
     const getUserLocation = async () => {//will ask user for permission to retrieve their location
@@ -28,9 +28,25 @@ const useLocation = () => {
 
             //this will return the location in a proper address, not coordinate points
             let address = await Location.reverseGeocodeAsync({
-                latitude, longitude
+                 latitude: latitude,
+                longitude:longitude
             })
-        
+
+            /*the expo reverse geolocation works by sending an array of the closest matching adresses
+          to the coordinate points this will choose the first candidate*/
+            if (result && result.length > 0) {
+            const place = result[0];
+        }
+            //this will return the address in a format acceptable by the google maps platform
+             const formattedAddress =  [
+                //prevents comma after the number from appearing
+                place.streetNumber + " " + place.street,
+                place.city,
+                place.region,
+                place.postalCode,
+                place.country,
+            ].filter(Boolean).join(", ")
+            setAddress(formattedAddress)
         }
     }
         useEffect(() => {
@@ -39,7 +55,7 @@ const useLocation = () => {
     
         
     
-    return {latitude, longitude, errorMsg}
+    return {latitude, longitude, errorMsg,address}
 }
 
 export default useLocation
